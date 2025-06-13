42bb77154747d6f82c27961b77d07a806766dbdd
mradul291 <mishramradul29@gmail.com>
2025-06-13 15:48:35 +0530

    Initial commit of Divine-admin Frappe app

---
+++ divine_admin/divine_admin_panel/doctype/product_image/test_product_image.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Pankaj Patidar and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestProductImage(FrappeTestCase):
+	pass
---
+++ divine_admin/divine_admin_panel/doctype/products/products.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Pankaj Patidar and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Products", {
+// 	refresh(frm) {
+
+// 	},
+// });
